6f730db30e42e07200aaccb478c7f98f4230a680
Tapiosir <tapio.sirvio11@gmail.com>
2017-10-10 12:03:04 +0300

    .

---
+++ bank-utilities/TestUtility.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace  Ekoodi.Utilities.Test 
+{
+    public static class TestUtility
+    {
+
+      public static void PrintGreetings()
+      {
+        Console.WriteLine("hello");
+
+      }
+
+
+    }
+
+}
---
+++ bban-validator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Ekoodi.Utilities.Test;
+
+namespace bban_validator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestUtility.PrintGreetings();
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
