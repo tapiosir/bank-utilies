a4cced14f1fa799ed3320ece3e274f714bd79085
Tapiosir <tapio.sirvio11@gmail.com>
2017-10-17 13:22:08 +0300

    committing bban validator

--- bank-utilities/TestUtility.cs
+++ bank-utilities/TestUtility.cs
@@ -6,20 +6,22 @@ namespace  Ekoodi.Utilities.Test
     public static class TestUtility
     {
 
-      public static void Validator()
-      {
+        public static void Validator()
+        {
 
             StringBuilder bban = new StringBuilder();
-
-            string bbanInput = "159030 - 776";
+            Console.WriteLine("please enter an account number");
+            string bbanInput = Console.ReadLine();
             string remove;
             string removeSpaces;
             remove = bbanInput.Replace("-", "");
             removeSpaces = remove.Replace(" ", "");
             bban.Append(removeSpaces);
-            int decider =int.Parse(bban[0].ToString());
+            int decider = int.Parse(bban[0].ToString());
+            int luhnCheckSum = 0;
+          
 
-            
+
             if (decider == 4)
             {
                 while (bban.Length < 14)
@@ -42,10 +44,37 @@ namespace  Ekoodi.Utilities.Test
                 }
             }
 
+            for (int i = 0; i <= 12; i++)
+            {
+                int asd = int.Parse(bban[i].ToString());
 
+                if (i % 2 == 1)
+                {
+                        luhnCheckSum = luhnCheckSum + asd;
+                }
+                else
+                {
+                    luhnCheckSum = luhnCheckSum +(asd * 2);
 
-            Console.WriteLine(bban);
-            Console.WriteLine(bban.Length);
+                   if (asd * 2 >= 10)
+                   {
+                      luhnCheckSum = luhnCheckSum - 9;
+                   }
+                }
+            }
+
+            int lastNumber = int.Parse(bban[13].ToString());
+            int test = (luhnCheckSum + lastNumber) % 10;
+            if (test == 0)
+            {
+                Console.WriteLine("account number {0} is correct", bban);
+            }
+            else
+            {
+                Console.WriteLine("account number was incorrect");
+            }
+
+           
             Console.ReadKey();
         }
 
